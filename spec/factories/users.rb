d79047cbf4d02d46dc68c53630550c974fd9220f
foagura <foagura_0416@yahoo.co.jp>
2024-05-02 13:56:58 +0900

    テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {Gimei.name.last}
+    first_name            {Gimei.name.first}
+    last_name_kana        {Gimei.name.last.katakana}
+    first_name_kana       {Gimei.name.first.katakana}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2019-12-31')}
+  end
+end
