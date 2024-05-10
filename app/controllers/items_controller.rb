950cab82fb96d8ca97a621e269b8925af33de5b3
foagura <foagura_0416@yahoo.co.jp>
2024-05-10 15:39:52 +0900

    createアクションの実装~jsに記述追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :content, :category_id, :condition_id,
-     :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(:user, current_user.id)
+    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id,
+     :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,14 +1,17 @@
-window.addEventListener('turbo:load', function(){
-  const itemPrice = document.getElementById("item-price");
-
-  itemPrice.addEventListener('input', function(){
-    const addTaxPrice = document.getElementById("add-tax-price");
-    const profit = document.getElementById("profit")
-    const itemPriceVal = itemPrice.value;
-    const addTaxPriceVal = itemPriceVal * 0.1;
-    const profitVal = itemPriceVal - addTaxPriceVal
-    
-    addTaxPrice.textContent = `${addTaxPriceVal}`;
-    profit.textContent = `${profitVal}`;
+'turbo:load turbo:render'.split(" ").forEach(function(eventName){
+  window.addEventListener(eventName, function(){
+    const itemPrice = document.getElementById("item-price");
+  
+    itemPrice.addEventListener('input', function(){
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const profit = document.getElementById("profit")
+      const itemPriceVal = itemPrice.value;
+      const addTaxPriceVal = itemPriceVal * 0.1;
+      const profitVal = itemPriceVal - addTaxPriceVal
+      
+      addTaxPrice.textContent = `${addTaxPriceVal}`;
+      profit.textContent = `${profitVal}`;
+    });
   });
 });
+
