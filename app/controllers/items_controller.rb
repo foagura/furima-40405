ae90a251d627a66ec7dd7ac2dcbd7348753919ff
foagura <foagura_0416@yahoo.co.jp>
2024-05-10 14:22:12 +0900

    ActiveHash~Javascript実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :content, :category_id, :condition_id,
+     :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(:user, current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :content, :price, :image, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
