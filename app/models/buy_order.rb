7e39f107208ddad2ae067f09b5aec7e6b04ebf7e
foagura <foagura_0416@yahoo.co.jp>
2024-05-31 16:07:55 +0900

    バリデーションにtokenカラムの追加

--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -5,7 +5,7 @@ class BuyOrder
 
 
   with_options presence: true do
-    validates :user_id, :item_id
+    validates :user_id, :item_id, :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\Z/, message: '[000-0000]のように半角で入力してください' }
     validates :municipalities, :address
     validates :phone_number, format: { with: /\A\d{10,11}\Z/, message: '10~11桁の半角数字で入力してください'}
